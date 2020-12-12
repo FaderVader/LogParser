3ce14d3ba3c171bc1a6f13528deaa709988eea71
FaderVader <jakobviggohansen@gmail.com>
2020-12-12 15:54:06 +0100

    moved tests - added conftest.py

--- src/app_test.py
+++ src/app_test.py
@@ -1,13 +1,13 @@
-from QueryParser import QueryParser
+# from QueryParser import QueryParser
 
-queryParser = QueryParser()
+# queryParser = QueryParser()
 
-# q = {"STARTEND": [["SendEvent", "DaletService", "Waiting"], ["SendEvent", "DaletService", "Success"]], "BETWEEN": ["2020-10-01-12:0:0.0", "2020-10-01-23:59:59.9"], "SORT":True}
-q = {"STARTEND": [["setupsession", "running"], ["setupsession", "completed"]], "CLIENT": "TX84402"}  # 
-# q = {"FIND": ["setupsession", "running"], "SORT": True}  # 
-# q = {"FIND": ["kestrel"]}  # 
-# q = {"STARTEND": [["setupsession", "running"], ["setupsession", "completed"]], "BETWEEN": ["2020-10-01-12:0:0.0", "2020-10-01-23:59:59.9"], "SHOWSTATS": 2}
+# # q = {"STARTEND": [["SendEvent", "DaletService", "Waiting"], ["SendEvent", "DaletService", "Success"]], "BETWEEN": ["2020-10-01-12:0:0.0", "2020-10-01-23:59:59.9"], "SORT":True}
+# q = {"STARTEND": [["setupsession", "running"], ["setupsession", "completed"]], "CLIENT": "TX84402"}  # 
+# # q = {"FIND": ["setupsession", "running"], "SORT": True}  # 
+# # q = {"FIND": ["kestrel"]}  # 
+# # q = {"STARTEND": [["setupsession", "running"], ["setupsession", "completed"]], "BETWEEN": ["2020-10-01-12:0:0.0", "2020-10-01-23:59:59.9"], "SHOWSTATS": 2}
 
-result = queryParser.Parse(q)
+# result = queryParser.Parse(q)
 
-print('Query complete.')
+# print('Query complete.')
